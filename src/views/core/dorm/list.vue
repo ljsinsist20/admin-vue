236cdcc2cdcfe41ece2ead159ea2cbf60247c275
ljsinsist20 <2593161116@qq.com>
2021-11-09 22:02:50 +0800

    3.增加查询功能

--- src/views/core/dorm/list.vue
+++ src/views/core/dorm/list.vue
@@ -1,35 +1,43 @@
 <template>
   <div class="app-container">
-       <el-table :data="list" border style="width: 100%" stripe>
-        <el-table-column prop="" label="#" width="50">
-          <template slot-scope="scope">
-            {{ (pageNum - 1) * pageSize + scope.$index + 1 }}
-          </template>
-        </el-table-column>
-        <el-table-column prop="name" label="宿舍名称"></el-table-column>
+
+    <el-form :inline="true" class="demo-form-inline">
+      <el-form-item label="宿舍名称">
+        <el-input v-model="searchObj.name" placeholder="宿舍名称"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" icon="el-icon-search" @click="fetchData()">查询</el-button>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="default" @click="resetData()">清除</el-button>
+      </el-form-item>
+    </el-form>
+
+    <el-table :data="list" border style="width: 100%" stripe>
+      <el-table-column prop="" label="#" width="50">
+        <template slot-scope="scope">
+          {{ (pageNum - 1) * pageSize + scope.$index + 1 }}
+        </template>
+      </el-table-column>
+      <el-table-column prop="name" label="宿舍名称"></el-table-column>
     </el-table>
-     <div class="block">
-      <el-pagination  @size-change="changePageSize" 
-                      @current-change="changeCurrentPage" 
-                      :current-page="pageNum" 
-                      :page-sizes="[5, 10, 20, 30]" 
-                      :page-size="pageSize" 
-                      layout="total, sizes, prev, pager, next, jumper" 
-                      :total="total">
+    <div class="block">
+      <el-pagination @size-change="changePageSize" @current-change="changeCurrentPage" :current-page="pageNum" :page-sizes="[5, 10, 20, 30]" :page-size="pageSize" layout="total, sizes, prev, pager, next, jumper" :total="total">
       </el-pagination>
     </div>
   </div>
 </template>
 
 <script>
-import dormAPI from "@/api/core/dorm.js"
+import dormAPI from '@/api/core/dorm.js'
 export default {
   data() {
     return {
       list: null,
       pageNum: 1,
       pageSize: 10,
-      total: 0
+      total: 0,
+      searchObj: {}
     }
   },
 
@@ -39,8 +47,9 @@ export default {
 
   methods: {
     fetchData() {
-      dormAPI.list(this.pageNum, this.pageSize).then((response) => 
-      {this.list = response.data.pageInfo.list, this.total = response.data.pageInfo.total})
+      dormAPI.list(this.pageNum, this.pageSize, this.searchObj).then(response => {
+        ;(this.list = response.data.pageInfo.list), (this.total = response.data.pageInfo.total)
+      })
     },
 
     changeCurrentPage(pageNum) {
@@ -51,11 +60,15 @@ export default {
     changePageSize(pageSize) {
       this.pageSize = pageSize
       this.fetchData()
+    },
+
+    resetData() {
+      this.searchObj = {}
+      this.fetchData()
     }
   }
 }
 </script>
 
 <style>
-
 </style>
